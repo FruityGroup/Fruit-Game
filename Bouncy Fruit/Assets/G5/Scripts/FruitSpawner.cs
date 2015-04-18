d1c26d94adf831c08a8ced744cf94ff46abe0e31
unknown <sxt5244@GDD-2.main.ad.rit.edu>
2015-04-18 17:40:44 -0400

    First Commit

---
+++ Bouncy Fruit/Assets/G5/Scripts/FruitSpawner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FruitSpawner : MonoBehaviour {
+
+    public float waitTime;
+    public GameObject[] fruits;
+
+	// Use this for initialization
+	void Start () {
+       // waitTime = 2.0f;
+	}
+	
+    IEnumerator spawnFruit()
+    {
+        int i = Random.Range(0, 4);
+
+        GameObject.Instantiate(fruits[i],this.transform.position, this.transform.rotation);
+
+        yield return new WaitForSeconds(waitTime);
+    }
+
+	// Update is called once per frame
+	void Update () {
+	  
+       spawnFruit();
+            
+	}
+}
